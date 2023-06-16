5b4c8969292c0aca5f67410bea046c0f459e4517
Giacomo <giacomo00.gf@gmail.com>
2023-06-16 12:04:35 +0200

    Revert "safe place"
    This reverts commit c18b61d0f6a9e755036a7dbea6bb10554068b65f.

--- include/Lente.hpp
+++ include/Lente.hpp
@@ -12,6 +12,7 @@ struct Lente{
 	Raggio Out_f (const Raggio& I) const;
 	void Log(std::ofstream &fpt) const;
 	Lente(Curva& inf, Curva& sup, double nd, double nf): Inf(inf), Sup(sup), N_d(nd), N_f(nf) {};
+	
 	//Lente& operator=(Lente&& bob);
 	//Lente(const Lente& source): Inf(source.Inf.Duplica()), Sup(source.Sup.Duplica()), N_d(source.N_d), N_f(source.N_f){};
 	//~Lente();     // frees the curves
